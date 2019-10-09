4e7547a79cee88a1289d91cfc862b4e1cc804f31
Lise Hove <lisehove@hotmail.com>
2019-10-09 18:41:50 -0300

    Complete the functions

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var calculadora = {
-    sumar: function sumarNumeros(n1, n2) {
-       //
-    },
+  sumar: function sumarNumeros(n1, n2) {
+    return n1 + n2;
+  },
 
-    restar: function restarNumeros(n1, n2) {
-       //
-       return (0);
-    },
-    
-    multiplicar: function multiplicarNumeros(n1, n2) {
-        //
-    },
+  restar: function restarNumeros(n1, n2) {
+    return n1 - n2;
+  },
 
-    dividir: function dividirNumeros(n1, n2) {
-        //
-    }
+  multiplicar: function multiplicarNumeros(n1, n2) {
+    return n1 * n2;
+  },
 
-
+  dividir: function dividirNumeros(n1, n2) {
+    return n1 / n2;
+  }
 };
