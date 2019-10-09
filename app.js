318ac784797bf2f2edbcb25926da588dced38ab6
Lise Hove <lisehove@hotmail.com>
2019-10-09 17:35:46 -0300

    agregar funcion sumar

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var calculadora = {
-    sumar: function sumarNumeros(n1, n2) {
-       //
-    },
+  sumar: function sumarNumeros(n1, n2) {
+    return n1 + n2;
+  },
 
-    restar: function restarNumeros(n1, n2) {
-       //
-    },
-    
-    multiplicar: function multiplicarNumeros(n1, n2) {
-        //
-    },
+  restar: function restarNumeros(n1, n2) {
+    //
+  },
 
-    dividir: function dividirNumeros(n1, n2) {
-        //
-    }
+  multiplicar: function multiplicarNumeros(n1, n2) {
+    //
+  },
 
-
+  dividir: function dividirNumeros(n1, n2) {
+    //
+  }
 };
